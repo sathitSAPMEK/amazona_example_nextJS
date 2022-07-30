19d27dcc4253da32eb23c6e50323a1980a2c6528
sathit sapmek <sathit.awb@gmail.com>
2022-07-30 09:49:52 +0700

    update component app

---
+++ pages/product/[slug].js
@@ -0,0 +1,19 @@
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
+import { data } from "../../utils/data";
+
+export default function ProductScreen() {
+  const router = useRouter();
+  const slug = router.query.slug;
+
+  const product = data.products.find((a) => a.slug === slug);
+
+  if (!product) {
+    return <div>Product Not Found</div>;
+  }
+  return (
+    <div>
+      <h1>{product.name}</h1>
+    </div>
+  );
+}
